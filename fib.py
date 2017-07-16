9c799673415d98720de1757240f930f64296a3b6
kerouanton <0github@9c.re>
2017-07-16 12:46:48 +0200

    added time check

--- fib.py
+++ fib.py
@@ -1,10 +1,17 @@
 #!/usr/local/bin/python3
+# -*- encoding: utf-8 -*-
 
-def fib(n):
-	a,b=0,1
-	while(a<n):
-		print(a,end=' ')
-		a,b=b,a+b
+import time
+
+def fib(n): # write Fibonacci series up to n
+	"""Print a Fibonacci series up to n."""
+	a, b = 0, 1
+	while(a < n):
+		print(a, end=' ')
+		a, b = b, a+b
 	print()
-
-fib(1e3)
+	
+	
+t=time.time()
+fib(1e200)
+print("Execution time:{} seconds.".format(time.time()-t))
