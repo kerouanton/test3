73d79aafa184bff77ddc043aa47dd94e535441f4
kerouanton <0github@9c.re>
2017-07-16 12:55:00 +0200

    200 to 201

--- fib.py
+++ fib.py
@@ -1,17 +1,18 @@
 #!/usr/local/bin/python3
-# -*- encoding: utf-8 -*-
+# coding: utf-8
 
 import time
 
 def fib(n): # write Fibonacci series up to n
-	"""Print a Fibonacci series up to n."""
-	a, b = 0, 1
-	while(a < n):
-		print(a, end=' ')
-		a, b = b, a+b
-	print()
-	
-	
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while(a < n):
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+
+
 t=time.time()
-fib(1e200)
+fib(1e201)
 print("Execution time:{} seconds.".format(time.time()-t))
